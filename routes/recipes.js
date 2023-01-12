540b043d9900aec2e907dec79f960e23a798adce
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-01-12 13:48:41 -0700

    started index.ejs

--- routes/recipes.js
+++ routes/recipes.js
@@ -14,6 +14,7 @@ router.get('/recipes', (req, res, next) => {
             throw err
         } else {
             console.log(data);
+            res.render('recipes/index', {recipes: data});
         }
     })
 });
@@ -24,18 +25,18 @@ router.get('/recipes/new', (req, res, _next) => {
     
     // this view should probably be renamed to something like '/views/recipes/new.ejs'
     res.render('submit_recipe');
-}
+});
            
 router.get('/recipes/:id', (req, res, _next) => {
     // 'show' route
     // This page displays the information for a single recipe with id matching the URL parameter
     // For example, `/recipes/10` will display the recipe with id=10
-}
+});
 
 router.get('/recipes/:id/edit', (req, res, _next) => {
     // 'edit' route
     // This page displays a form to edit a specific recipe with id matching the URL parameter
-}
+});
            
 router.post('/recipes/', (req, res, _next) => {
     // 'create' route
@@ -48,11 +49,11 @@ router.post('/recipes/', (req, res, _next) => {
             console.log('data inserted into database');
         res.redirect('/recipes');
     })
-}
+});
 
 router.post('/recipes/:id', (req, res, _next) => {
     // 'update' route
     // This route accepts data from the form at '/recipes/:id/edit' and updates the record in the database
-}
+});
 
 module.exports = router;
