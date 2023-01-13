69e2b760b5a8957e831d7afbb96c19cc758ab6bd
Bryce Richey <107497353+BryceRichey@users.noreply.github.com>
2023-01-13 13:38:32 -0700

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '***REMOVED***',
+    password: '...',
     database: 'project_1',
     port: '3306'
 })
