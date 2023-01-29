ad4c20d0b22ec216cac734135daa66694e0c05cf
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-01-29 16:46:05 -0700

    Partially implement passport.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = require('../app');
+const session = require('express-session');
 const router = express.Router();
-const db = require('../database');
+const app = require('../app');
+const db = require('../config/database');
+const passport = require('../config/passport');
 
 
 router.get('/sign_up', (req, res, next) => {
@@ -16,23 +18,44 @@ router.post('/sign_up', (req, res, next) => {
         email: req.body.email,
         p_word: req.body.p_word
     }
-    db.query('SELECT * FROM users WHERE email = ?', [sign_up.email], (err, data) => {
+    const saltHash = genPassword(req.body.p_word);
+    const salt = saltHash.salt;
+    const hash = saltHash.hash;
+    db.query('INSERT INTO users(u_name, has, salt, isAdmin) values(?, ?, ?, 0)', [req.body.u_name, hash, salt], (err, data, fileds) => {
         if (err) {
-            throw err
-        } if (data.length > 1) {
-            msg = 'An account with' + sign_up.email + 'already exists!'
+            console.log(err);
         } else {
-            db.query('INSERT INTO users SET ?', sign_up, (err, data) => {
-                if (err) {
-                    throw err
-                } else {
-                    msg = 'Account created successfully'
-                }
-                res.redirect('/login')
-            })
+            console.log('Successfully created an account')
         }
-    })
+    });
+    res.redirect('/login')
 });
+
+// router.post('/sign_up', (req, res, next) => {
+//     sign_up = {
+//         f_name: req.body.f_name,
+//         l_name: req.body.l_name,
+//         u_name: req.body.u_name,
+//         email: req.body.email,
+//         p_word: req.body.p_word
+//     }
+//     db.query('SELECT * FROM users WHERE email = ?', [sign_up.email], (err, data) => {
+//         if (err) {
+//             throw err
+//         } if (data.length > 1) {
+//             msg = 'An account with' + sign_up.email + 'already exists!'
+//         } else {
+//             db.query('INSERT INTO users SET ?', sign_up, (err, data) => {
+//                 if (err) {
+//                     throw err
+//                 } else {
+//                     msg = 'Account created successfully'
+//                 }
+//                 res.redirect('/login')
+//             })
+//         }
+//     })
+// });
 
 router.get('/login', (req, res, next) => {
     res.render('users/login.ejs')
@@ -55,7 +78,7 @@ router.post('/login', (req, res, next) => {
 });
 
 router.get('/logout', (req, res, next) => {
-    req.session.destroy();
+    req.logout();
     res.redirect('/login');
 });
 
