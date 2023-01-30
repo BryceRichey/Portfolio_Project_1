68579479189b71c8a897d724e9405f57ecf5e152
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-01-30 12:42:09 -0700

    Partially implement passport.js in login page

--- routes/users.js
+++ routes/users.js
@@ -5,23 +5,21 @@ const app = require('../app');
 const db = require('../config/database');
 const passport = require('../config/passport');
 
-
 router.get('/sign_up', (req, res, next) => {
     res.render('users/sign_up.ejs')
 });
 
-router.post('/sign_up', (req, res, next) => {
+router.post('/sign_up', [passport.userExists, (req, res, next) => {
     sign_up = {
         f_name: req.body.f_name,
         l_name: req.body.l_name,
         u_name: req.body.u_name,
-        email: req.body.email,
-        p_word: req.body.p_word
+        email: req.body.email
     }
     const saltHash = genPassword(req.body.p_word);
     const salt = saltHash.salt;
     const hash = saltHash.hash;
-    db.query('INSERT INTO users(u_name, has, salt, isAdmin) values(?, ?, ?, 0)', [req.body.u_name, hash, salt], (err, data, fileds) => {
+    db.query('INSERT INTO users(f_name, l_name, u_name, email, hash, salt, isAdmin) values(?, ?, ?, ?, ?, ?, 0)', [...Object.values(sign_up), hash, salt], (err, data, fileds) => {
         if (err) {
             console.log(err);
         } else {
@@ -29,53 +27,33 @@ router.post('/sign_up', (req, res, next) => {
         }
     });
     res.redirect('/login')
-});
-
-// router.post('/sign_up', (req, res, next) => {
-//     sign_up = {
-//         f_name: req.body.f_name,
-//         l_name: req.body.l_name,
-//         u_name: req.body.u_name,
-//         email: req.body.email,
-//         p_word: req.body.p_word
-//     }
-//     db.query('SELECT * FROM users WHERE email = ?', [sign_up.email], (err, data) => {
-//         if (err) {
-//             throw err
-//         } if (data.length > 1) {
-//             msg = 'An account with' + sign_up.email + 'already exists!'
-//         } else {
-//             db.query('INSERT INTO users SET ?', sign_up, (err, data) => {
-//                 if (err) {
-//                     throw err
-//                 } else {
-//                     msg = 'Account created successfully'
-//                 }
-//                 res.redirect('/login')
-//             })
-//         }
-//     })
-// });
+}]);
 
 router.get('/login', (req, res, next) => {
     res.render('users/login.ejs')
 });
 
-router.post('/login', (req, res, next) => {
-    const email = req.body.email;
-    const p_word = req.body.p_word;
-    db.query('SELECT * FROM users WHERE email = ? AND p_word = ?', [email, p_word], (err, data) => {
-        if (err) {
-            throw err
-        } else if (data.length > 0) {
-            req.session.loggedIn = true;
-            req.session.email = email;
-            res.redirect('/');
-        } else {
-            res.render('users/login')
-        }
-    })
-});
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureMessage: true
+}));
+
+// router.post('/login', (req, res, next) => {
+//     const email = req.body.email;
+//     const p_word = req.body.p_word;
+//     db.query('SELECT * FROM users WHERE email = ? AND p_word = ?', [email, p_word], (err, data) => {
+//         if (err) {
+//             throw err
+//         } else if (data.length > 0) {
+//             req.session.loggedIn = true;
+//             req.session.email = email;
+//             res.redirect('/');
+//         } else {
+//             res.render('users/login')
+//         }
+//     })
+// });
 
 router.get('/logout', (req, res, next) => {
     req.logout();
