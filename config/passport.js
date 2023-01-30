0fd358403bc0e8dbb681aa5fdc6c2af155d64baf
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-01-30 15:42:03 -0700

    Implemented working passport.js to login page

--- config/passport.js
+++ config/passport.js
@@ -4,24 +4,59 @@ const crypto = require('crypto');
 
 const db = require('./database');
 
-validPassword = (password, hash, salt) => {
-    const hashVerify = crypto
-        .pbkdf2Sync(password, salt, 10000, 60, 'sha512')
-        .toString('hex');
-    return hash === hashVerify;
+const verifyCallback = (username, password, done) => {
+    db.query('SELECT * FROM users WHERE email = ?', [username], (err, data, fields) => {
+        if (err) {
+            return done(err);
+        }
+        if (data.lenght == 0) {
+            return done(null, false, { message: 'Incorrect email or password.' });
+        }
+
+        const isValid = validPassword(password, data[0].hash, data[0].salt);
+
+        user = { id: data[0].id, username: data[0].username, hash: data[0].hash, salt: data[0].salt }
+
+        if (isValid) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    });
 }
+
+const strategy = new LocalStrategy({usernameField: 'email'}, verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    db.query('SELECT * FROM user WHERE id = ?', [id], (err, data) => {
+        done(null, data[0])
+    });
+});
 
 genPassword = (password) => {
     const salt = crypto
         .randomBytes(32)
         .toString('hex');
     const genhash = crypto
-        .pbkdf2Sync(password, salt, 10000, 60, 'sha512')
+        .pbkdf2Sync(password, salt, 10000, 64, 'sha512')
         .toString('hex');
     return {
         salt: salt,
         hash: genhash
     };
+}
+
+validPassword = (password, hash, salt) => {
+    const hashVerify = crypto
+        .pbkdf2Sync(password, salt, 10000, 64, 'sha512')
+        .toString('hex');
+    return hash === hashVerify;
 }
 
 isAuth = (req, res, next) => {
@@ -41,7 +76,7 @@ isAdmin = (req, res, next) => {
 }
 
 userExists = (req, res, next) => {
-    db.query('SELECT * FROM users WHERE u_name = ?', [req.body.u_name], (err, data, fields) => {
+    db.query('SELECT * FROM users WHERE username = ?', [req.body.username], (err, data, fields) => {
         if (err) {
             console.log(err);
         } else if (data.length > 0) {
@@ -51,47 +86,5 @@ userExists = (req, res, next) => {
         }
     });
 }
-
-const customFields = {
-    email: 'email',
-    username: 'u_name',
-    password: 'p_word'
-}
-
-const verifyCallback = (username, password, done) => {
-    db.query('SELECT * FROM users WHERE u_name = ?', [username], (err, data, fields) => {
-        if (err) {
-            return done(err);
-        }
-        if (data.length == 0) {
-            return done(null, false, { message: 'Incorrect username or password.' });
-        }
-        const isValid = validPassword(
-            password, data[0].hash, data[0].salt);
-        user = {
-            id: data[0].id, username: data[0].username, hash: data[0].hash, salt: data[0].salt
-        };
-        if (isValid) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-        }
-    });
-}
-
-const strategy = new LocalStrategy(customFields, verifyCallback);
-
-passport.use(strategy);
-
-passport.serializeUser((user, done) => {
-    done(null, user.id)
-});
-
-passport.deserializeUser((id, done) => {
-    db.query('SELECT * FROM user WHERE id = ?', [id], (err, data) => {
-        done(null, data[0])
-    });
-});
-
 
 module.exports = { passport, isAuth, isAdmin, userExists };
--- routes/users.js
+++ routes/users.js
@@ -14,7 +14,7 @@ router.post('/sign_up', [passport.userExists, (req, res, next) => {
         username: req.body.username,
         email: req.body.email
     }
-    const saltHash = genPassword(req.body.p_word);
+    const saltHash = genPassword(req.body.password);
     const salt = saltHash.salt;
     const hash = saltHash.hash;
     db.query('INSERT INTO users(f_name, l_name, username, email, hash, salt, isAdmin) values(?, ?, ?, ?, ?, ?, 0)', [...Object.values(sign_up), hash, salt], (err, data, fileds) => {
@@ -31,27 +31,8 @@ router.get('/login', (req, res, next) => {
     res.render('users/login.ejs')
 });
 
-router.post('/login', passport.passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureMessage: true
-}));
+router.post('/login', passport.passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureMessage: true }));
 
-// router.post('/login', (req, res, next) => {
-//     const email = req.body.email;
-//     const p_word = req.body.p_word;
-//     db.query('SELECT * FROM users WHERE email = ? AND p_word = ?', [email, p_word], (err, data) => {
-//         if (err) {
-//             throw err
-//         } else if (data.length > 0) {
-//             req.session.loggedIn = true;
-//             req.session.email = email;
-//             res.redirect('/');
-//         } else {
-//             res.render('users/login')
-//         }
-//     })
-// });
 
 router.get('/logout', (req, res, next) => {
     req.logout();
