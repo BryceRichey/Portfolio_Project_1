8748cbfe6765f6146ff87a4aea48f60d72864d69
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-02-08 12:15:07 -0700

    Corrected signup_validations script

--- public/javascripts/signup_validations.js
+++ public/javascripts/signup_validations.js
@@ -1,24 +1,28 @@
-const updateNames = document.querySelector('#newNames');
+const createAccount = document.querySelector('#createAccount');
 
-updateNames.addEventListener('submit', e => {
-    validateNames();
+createAccount.addEventListener('submit', e => {
+    validateAccount();
 });
 
-function validateNames() {
-    const newFirstName = document.getElementById('f_name').value.trim();
-    const newLastName = document.getElementById('l_name').value.trim();
-    const newUsername = document.getElementById('username').value.trim();
+function validateAccount() {
+    const firstName = document.getElementById('f_name').value.trim();
+    const lastName = document.getElementById('l_name').value.trim();
+    const username = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
 
-    if (newFirstName == "") {
+    if (firstName == "") {
         alert('First name cannot be left blank');
         return false;
-    } else if (newLastName == "") {
+    } else if (lastName == "") {
         alert('Last name cannot be left blank');
         return false;
-    } else if (newUsername == "") {
+    } else if (username == "") {
         alert('Username cannot be left blank')
         return false;
-    } else if (newFirstName == "" && newLastName == "") {
+    } else if (email == "") {
+        alert('Email cannot be left blank')
+        return false;
+    } else if (firstName == "" && fastName == "") {
         alert('First and last name cannot be left blank')
         return false;
     } else {
