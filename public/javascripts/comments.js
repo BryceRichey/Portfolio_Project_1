97717e6feeba93e9a69a0c68aab2f0c743396bdf
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-03-08 14:35:05 -0800

    Added comment button to recipes, textbox and submit button dynamically appear

---
+++ public/javascripts/comments.js
@@ -0,0 +1,19 @@
+const createComment = document.querySelector('#createComment');
+
+createComment.addEventListener('click', e => {
+    e.preventDefault;
+    showTextArea();
+})
+
+function showTextArea() {
+    const commentForm = document.getElementById('commentForm');
+    const textArea = document.createElement('textarea');
+    const submitButton = document.createElement('button');
+
+    commentForm.textContent = '';
+    commentForm.appendChild(textArea);
+    textArea.classList.add('form-control');
+    commentForm.appendChild(submitButton).innerText = 'Submit';
+    submitButton.classList.add('btn', 'btn-dark', 'mt-3');
+    submitButton.setAttribute('type', 'submit');
+}
