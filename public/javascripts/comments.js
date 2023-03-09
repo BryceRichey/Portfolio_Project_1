c1b2790074cf648c2b2a2ef2010ccdab4ffc8ae8
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-03-08 18:06:49 -0800

    Added ability to comment on a recipe and insert data into database

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -3,7 +3,7 @@ const createComment = document.querySelector('#createComment');
 createComment.addEventListener('click', e => {
     e.preventDefault;
     showTextArea();
-})
+});
 
 function showTextArea() {
     const commentForm = document.getElementById('commentForm');
@@ -13,6 +13,8 @@ function showTextArea() {
     commentForm.textContent = '';
     commentForm.appendChild(textArea);
     textArea.classList.add('form-control');
+    textArea.setAttribute('name', 'comment');
+
     commentForm.appendChild(submitButton).innerText = 'Submit';
     submitButton.classList.add('btn', 'btn-dark', 'mt-3');
     submitButton.setAttribute('type', 'submit');
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,69 +2,80 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 
-router.get('/recipes', (req, res, next) => {
+router.get('/recipes', (_req, res, _next) => {
     db.query('SELECT * FROM recipes ORDER BY id DESC', (err, data) => {
         if (err) {
             throw err
         } else {
             res.render('recipes/index', { recipes: data });
         }
-    })
+    });
 });
 
-router.get('/recipes/new', (req, res, next) => {
+router.get('/recipes/new', (_req, res, _next) => {
     res.render('recipes/new');
 });
 
-router.get('/recipes/:id', (req, res, next) => {
+router.get('/recipes/:id', (req, res, _next) => {
     db.query(`SELECT id, r_title, num_serv, ingredients, directions FROM recipes WHERE id = ${req.params.id}`, (err, data) => {
         if (err) {
             throw err
         } else {
             res.render('recipes/show', { recipe: data });
         }
-    })
+    });
 });
 
-router.get('/recipes/:id/edit', (req, res, next) => {
+router.get('/recipes/:id/edit', (req, res, _next) => {
+    console.log(req.params.id)
     db.query(`SELECT id, r_title, num_serv, ingredients, directions FROM recipes WHERE id = ${req.params.id}`, (err, data) => {
         if (err) {
             throw err
         } else {
             res.render('recipes/edit', { id: req.params.id, recipe: data });
         }
-    })
+    });
 });
 
-router.post('/recipes/new', (req, res, next) => {
-    db.query('INSERT INTO recipes SET ?', { r_title: req.body.r_title, num_serv: req.body.num_serv, ingredients: req.body.ingredients, directions: req.body.directions }, (err, result) => {
+router.post('/recipes/new', (req, res, _next) => {
+    db.query(`INSERT INTO recipes SET ?`, { r_title: req.body.r_title, num_serv: req.body.num_serv, ingredients: req.body.ingredients, directions: req.body.directions }, (err, _result) => {
         if (err) {
             throw err
         } else {
             console.log('data inserted into database');
             res.redirect('/recipes');
         }
-    })
+    });
 });
 
-router.post('/recipes/:id/edit', (req, res, next) => {
-    db.query(`UPDATE recipes SET ? WHERE id = ${req.params.id}`, { r_title: req.body.r_title, num_serv: req.body.num_serv, ingredients: req.body.ingredients, directions: req.body.directions }, (err, data) => {
+router.post('/recipes/comment/:id', (req, res, _next) => {
+    db.query(`INSERT INTO comments SET ?`, { recipe_id: req.params.id, user_id: req.user.id, comment: req.body.comment }, (err, _data) => {
         if (err) {
             throw err
         } else {
-            res.redirect(`/recipes/${req.params.id}`)
+            res.redirect(`/recipes/${req.params.id}`);
         }
-    })
+    });
 });
 
-router.get('/recipes/:id/delete', (req, res, next) => {
-    db.query(`DELETE FROM recipes WHERE id = ${req.params.id}`, (err, data) => {
+router.post('/recipes/:id/edit', (req, res, _next) => {
+    db.query(`UPDATE recipes SET ? WHERE id = ${req.params.id}`, { r_title: req.body.r_title, num_serv: req.body.num_serv, ingredients: req.body.ingredients, directions: req.body.directions }, (err, _data) => {
         if (err) {
             throw err
         } else {
-            res.redirect(`/recipes`)
+            res.redirect(`/recipes/${req.params.id}`);
         }
-    })
+    });
+});
+
+router.get('/recipes/:id/delete', (req, res, _next) => {
+    db.query(`DELETE FROM recipes WHERE id = ${req.params.id}`, (err, _data) => {
+        if (err) {
+            throw err
+        } else {
+            res.redirect(`/recipes`);
+        }
+    });
 });
 
 module.exports = router;
