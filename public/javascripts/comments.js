7b3d6a802cb213a2b2bb9c8fe65704aca4874f1e
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-04-24 12:09:43 -0700

    Cleaned up code

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -2,12 +2,7 @@ const createComment = document.querySelector('#createComment');
 
 createComment.addEventListener('click', e => {
     showTextArea();
-    checkIfUser();
 });
-
-function checkIfUser() {
-    console.log();
-}
 
 function showTextArea() {
     const commentForm = document.getElementById('commentForm');
