14aea69e83ba3f47c1e3309036237cc124a63b0b
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-06-01 12:49:05 -0700

    Refactoring sql queries

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const dayjs = require('dayjs');
 const cloudinary = require('../config/cloudinary');
+const recipeQueries = require('../db/recipes');
 
 
 router.get('/recipes', (_req, res, _next) => {
@@ -20,33 +21,20 @@ router.get('/recipes/new', (_req, res, _next) => {
     res.render('recipes/new');
 });
 
+
+
+
 router.get('/recipes/:id', async (req, res, _next) => {
-    const recipeShowPage = async () => {
-        
-        const queryTwo = `SELECT comments.*, COUNT(l.id) AS likes FROM comments LEFT JOIN likes l ON comments.id = l.comment_id WHERE recipe_id = ${req.params.id} GROUP BY comments.id`
+    const recipe = await recipeQueries.getRecipe(req.params.id);
+    const comments = await recipeQueries.getRecipeComments(req.params.id);
+    const commentLikes = await recipeQueries.getUserRecipeCommentLikes(req.params.id, req.user);
+    const photos = await recipeQueries.getRecipePhotos(req.params.id);
 
-        const queryFour = `SELECT * FROM recipe_photos rp WHERE rp.recipe_id = ${req.params.id}`
-
-        let commentLikes = [];
-        if (req.user && req.user.id) {
-            const queryThree = `SELECT comments.id FROM likes LEFT JOIN comments ON likes.comment_id = comments.id WHERE recipe_id = ${req.params.id} and likes.user_id = ${req.user.id}`;
-            let [commentLikes, _responseThreefields] = await db.promise().query(queryThree);
-
-            commentLikes = commentLikes.map((row) => Object.values(row)[0]);
-        }
-
-        
-        const [comments, _responseTwofields] = await db.promise().query(queryTwo);
-        const [photos, _responseFourFields] = await db.promise().query(queryFour);
-
-        return { recipe, comments, commentLikes, photos };
-    }
-
-    let results = await recipeShowPage();
-    results['dayjs'] = dayjs;
-
-    res.render('recipes/show', results);
+    res.render('recipes/show', { recipe, comments, commentLikes, photos, dayjs });
 });
+
+
+
 
 router.get('/recipes/:id/edit', (req, res, _next) => {
     db.query(`SELECT * FROM recipes WHERE id = ${req.params.id}`, (err, data) => {
@@ -61,46 +49,18 @@ router.get('/recipes/:id/edit', (req, res, _next) => {
     });
 });
 
+
+
+
 router.post('/recipes/new', cloudinary.upload.single('photo'), async (req, res, _next) => {
-    const postNewRecipe = async (ingredientArray) => {
-        const queryOne = `INSERT INTO recipes SET ?`
-        const queryTwo = `SELECT LAST_INSERT_ID()`
-        const queryThree = `INSERT INTO ingredients (ingredient) VALUES ?`
-        const queryFour = `INSERT INTO recipe_photos SET ?`
-
-
-        const [_reponseOneRows, _responseOnefields] = await db.promise().query(queryOne, {
-            submit_user_id: req.user.id,
-            submit_user_first_name: req.user.f_name,
-            submit_user_last_name: req.user.l_name,
-            r_title: req.body.r_title,
-            servings: req.body.num_serv,
-            prep_time: req.body.prep_time,
-            cook_time: req.body.cook_time,
-            description: req.body.description,
-            directions: req.body.directions
-        });
-        const [reponseTwoRows, _responseTwofields] = await db.promise().query(queryTwo);
-        const newRecipeId = (Object.values(reponseTwoRows[0]));
-        const [_responseThreeRows, _responseThreeFieleds] = await db.promise().query(queryThree, [ingredientArray]);
-        const [_reponseThreeRows, _responseThreefields] = await db.promise().query(queryFour, {
-            recipe_id: newRecipeId,
-            user_id: req.user.id,
-            photo_url: req.file.path
-        });
-    }
-
-    let ingredients = req.body.ingredients;
-    let ingredientArray = Array();
-    ingredients.forEach(ingredient => {
-        ingredientArray.push([ingredient.ingredient]);
-    });
-    //const ingredientsString = ingredientArray;
-
-    postNewRecipe(ingredientArray);
+    await recipeQueries.createRecipe();
+    await recipeQueries.createRecipeIngredient(recipe_id);
 
     res.redirect('/recipes');
 });
+
+
+
 
 router.post('/recipes/comment/:id', (req, res, _next) => {
     db.query(`INSERT INTO comments SET ?`, {
