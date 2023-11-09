18db9efc5187cfe5bc92962404a34e599d656a40
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2023-11-09 08:51:28 -0700

    added dockerfile and fly.io files

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 const mysql2 = require('mysql2');
 
 const db = mysql2.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'project_1',
+    host: process.env.DATABASE_URL || 'localhost',
+    user: process.env.DATABASE_USER || 'root',
+    database: process.env.DATABASE_NAME || 'project_1',
     port: '3306',
     password: process.env.DB_PASS,
     waitForConnections: true,
